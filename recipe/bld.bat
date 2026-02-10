1e6d2b2025b7e2bf2052cf459ee2bb6be2909715
Diego Ferigo <dferigo@rai-inst.com>
2026-02-10 23:14:39 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -31,7 +31,7 @@ if "%cuda_compiler_version%"=="None" (
 
 :: Install the project into cwd.
 :: This is needed only to produce the exported CMake targets.
-cmake --install build-ci --config Release --prefix "install-ci"
+cmake --install build-ci/Release --prefix "install-ci"
 
 :: In theory there should be only one wheel
 for %%F in (build-ci\Release\dist\onnxruntime*.whl) do (
